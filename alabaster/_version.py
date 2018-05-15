593aa8280de7e695ee0eeb529c209d82549fce43
Steven Loria <sloria1@gmail.com>
2018-05-15 14:55:08 -0400

    Use version identifier to specify fork version

--- alabaster/_version.py
+++ alabaster/_version.py
@@ -1,2 +1,2 @@
-__version_info__ = (0, 7, 10)
-__version__ = '.'.join(map(str, __version_info__))
+__version_info__ = (0, 7, 10, '+sloria0')
+__version__ = '.'.join(map(str, __version_info__[:3])) + __version_info__[3]
